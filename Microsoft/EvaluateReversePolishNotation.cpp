17a111fc4f437b5ab2130a0a5fa6427bc00f45de
Sania Ahmad <94756953+saniaahmad6@users.noreply.github.com>
2023-01-01 14:07:18 +0530

    Solved Question 1
    You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
    Link: https://leetcode.com/problems/evaluate-reverse-polish-notation/submissions/868941002/

---
+++ Microsoft/EvaluateReversePolishNotation.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack <int> st;
+        int n=tokens.size();
+        
+        for (int i=0;i<n;i++){
+            if (!(tokens[i]=="+" || tokens[i]=="-" || tokens[i]=="*" ||tokens[i]=="/")){
+                st.push(stoi(tokens[i]));
+            }
+            else{
+                if (tokens[i]=="+"){
+                    int op1=st.top();
+                    st.pop();
+                    int op2=st.top();
+                    st.pop();
+                    
+                    st.push(op2+op1);
+                    
+                }
+                else if (tokens[i]=="-"){
+                    int op1=st.top();
+                    st.pop();
+                    int op2=st.top();
+                    st.pop();
+                    st.push(op2-op1);
+                }
+                else if (tokens[i]=="*"){
+                    int op1=st.top();
+                    st.pop();
+                    int op2=st.top();
+                    st.pop();
+                    st.push(op2*op1);
+                }
+                else if (tokens[i]=="/"){
+                    int op1=st.top();
+                    st.pop();
+                    int op2=st.top();
+                    st.pop();
+                    st.push(op2/op1);
+                    
+                }
+               
+            }
+            
+        }
+     
+        
+        return st.top();
+    }
+};
