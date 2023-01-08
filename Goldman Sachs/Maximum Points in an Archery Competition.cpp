200334c6d61cbc8f5e0b32bcb888a424e75fd493
Sania Ahmad <94756953+saniaahmad6@users.noreply.github.com>
2023-01-08 21:19:37 +0530

    Maximum Points in an Archery Competition
    Submission Link: https://leetcode.com/problems/maximum-points-in-an-archery-competition/submissions/874145106/

---
+++ Goldman Sachs/Maximum Points in an Archery Competition.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector <int> v;
+    int ans=0;
+    void help (int index,int stock,vector<int>& alice,int currsum,vector <int> bob){
+        if (index==12 || stock<=0){
+            //reached end
+            if (currsum > ans){
+                if (stock>0){
+                bob[11] +=stock;
+                
+            }
+                ans=currsum; v=bob;
+            }
+            
+            return;
+        }
+        
+        //pick
+        int req=alice[index]+1;
+        if (req<=stock){
+            bob[index]=req;
+            help(index+1,stock-req,alice,currsum+index,bob);
+            bob[index]=0;
+        }
+        //notpick
+        help (index+1,stock,alice,currsum,bob);
+
+    }
+
+    vector<int> maximumBobPoints(int numArrows, vector<int>& aliceArrows) {
+        vector <int> bob(12,0);
+        help(0,numArrows,aliceArrows,0,bob);
+        return v;
+
+    }
+};
